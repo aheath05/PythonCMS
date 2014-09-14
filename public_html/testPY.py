ce28c114d5a894d87ffea16c7e96886c08a58014
aheath05 <alicia@coveycs.com>
2014-09-14 15:25:51 -0400

    changes to index.html and added css to page

--- public_html/testPY.py
+++ public_html/testPY.py
@@ -1,22 +1,17 @@
-
-# -*- coding: UTF-8 -*-
 #!/usr/bin/env python
 # enable debugging
 import cgitb; 
 cgitb.enable()
-import sys
-import web
+#import sys
+#import web
 
+print "Content-Type: text/html"
+print
+print """\
+<html>
+<body>
+<h2>Hello World!</h2>
+</body>
+</html>
+"""
 
-urls = (
-  '/', 'index'
-)
-
-print "Content-Type: text/plain;charset=utf-8"
-print "Hello World!"
-
-f = open('index.html', 'r')
-print f
-f.read()
-for line in f:
-	print line,
