23df619d619e2d1b0ef3d0620c8010127c240daa
root <moorek2@sunyit.edu>
2014-09-23 19:38:07 -0400

    changed index.py

---
+++ index.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# enable debugging
+import cgitb; 
+cgitb.enable()
+#import sys
+#import web
+
+print "Content-Type: text/html"
+print
+print """\
+<html>
+<body>
+<h2>Hello World!</h2>
+</body>
+</html>
+"""
+
