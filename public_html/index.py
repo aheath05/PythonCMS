c71d2f4640fa0dc1e532a3921ee31621e8b7624b
aheath05 <alicia@coveycs.com>
2014-09-23 19:16:03 -0400

    added .p files

---
+++ public_html/index.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# enable debugging
+import cgitb; 
+cgitb.enable()
+#import sys
+#import web
+
+print "Content-Type: text/html"
+print
+print """\
+<html>
+<body>
+<h2>Page2 inside html folder</h2>
+</body>
+</html>
+"""
+
