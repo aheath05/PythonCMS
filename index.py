492ce3bf78e05fa4840ce560af27fee920a9b59a
Kyler Moore <moorek2@sunyit.edu>
2014-12-03 10:18:10 -0500

    Update index.py

--- index.py
+++ index.py
@@ -12,22 +12,18 @@ cgitb.enable()
 
 
 def md2html(filename):
-
-	sys.path.append('Markdown-2.5')
-	old_stdout = sys.stdout
-	#sys.stdout = open('out.html', 'w')
-	result = StringIO()
-	sys.stdout = result
-
-
-	if __name__ == '__main__':
-	    sys.argv.append(filename)
-	    from markdown.__main__ import run
-	    run()
-
-	sys.stdout = old_stdout
-	result_string = result.getvalue()
-	print result_string
+  sys.path.append('Markdown-2.5/')
+  old_stdout = sys.stdout
+  #sys.stdout = open('out.html', 'w')
+  result = StringIO()
+  sys.stdout = result
+  if __name__ == '__main__':
+    sys.argv.append(filename)
+    from markdown.__main__ import run
+    run()
+  sys.stdout = old_stdout
+  result_string = result.getvalue()
+  print result_string
 
 
 print "Content-Type: text/html;charset=utf-8"
@@ -39,8 +35,7 @@ print "		<meta name='viewport' content='width=device-width, initial-scale=1'>"
 print "        <link rel='stylesheet' href='home/default.css' />" 
 print "        <link rel='stylesheet' href='home/main.css' >"
 print "        <link rel='stylesheet' href='home/responsive.css' media='screen' >"
-# call the header and sidebar
-modules.modules()
+
 
 req_uri = os.environ["REQUEST_URI"]
 cont_prefix = os.environ["CONTEXT_PREFIX"]
@@ -49,8 +44,10 @@ data = {}
 dir_bool = False
    
 site_path = req_uri.replace(cont_prefix, "", 1)
+if not site_path == "/" and not site_path.endswith('.md'):
+  site_path = site_path+".md"
 
-
+# For comparing site_path to directories in content folder
 for dir_entry in os.listdir(path):
   dir_entry = "/%s" % (dir_entry)
   if site_path == dir_entry:
@@ -58,36 +55,18 @@ for dir_entry in os.listdir(path):
     break
  
 if site_path == "/":
-  f = open("content/index.md")
-  file_contents = f.read()
-  print (file_contents)
-  f.close()
-#elif dir_bool == True and dir_entry.endswith('.jpg'):
-#  urllib2.urlopen("http://pythoncms.com/content"+dir_entry)
-elif dir_bool == True and dir_entry.endswith('.md'):
-  filename = "content"+dir_entry
-  md2html("/var/www/pythoncms.com/content"+dir_entry)
-  #print content
+  # call the header and sidebar
+  modules.modules()
+  md2html("content/index.md")
+  modules.footer()
 elif dir_bool == True:
-  f = open("content"+dir_entry, 'r')
-  file_contents = f.read()
-  print (file_contents)
-  f.close()
+  # call the header and sidebar
+  modules.modules()
+  md2html("content"+site_path)
+  modules.footer()
 else:
-  print "<h5>    <em style='color:#2F7288; '>(The link you requested either does not exist or you do not have permission to access it. Better luck next time!)    </em></h5>"
-  f = open("content/index.md")
-  file_contents = f.read()
-  print (file_contents)
-  f.close()
+  modules.header()
+  print "<div style=\"height:400px;text-align:center;\">    <h5>      <em style='color:#2F7288; '>        The page you requested does not exsist or you do not have permission to view it.<br/>        If neither of these are true please contact the server administrator.      </em>    </h5>  </div>"
+  modules.footer()
 
-
-
-# Uncomment the two lines below to see all web variables for this page request
-#
-      
-#for param in os.environ.  keys():
-#  print "<p><b>%20s</b>: %s</p>" % (param, os.environ[param])
-
-#call the footer
-modules.footer()                
 print "</html>"
